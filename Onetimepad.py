bb6140734a0bfc11088c4b7d2c4150d588a9c080
neeraj <neerajchowdary889@gmail.com>
2022-08-08 22:56:15 +0530

    Neeraj Push onetimepad and shift

--- Onetimepad.py
+++ Onetimepad.py
@@ -4,15 +4,9 @@ import onetimepad
 str = input("Enter Plaintext to Encrypt: ")
 i = 0
 key = ''.join([random.choice(string.ascii_letters) for i in range(len(str))])
-print(str, key)
 Ciphertext = onetimepad.encrypt(str, key)
-<<<<<<< HEAD
 print("Ciphertext is: \n"+Ciphertext)
 plaintext = onetimepad.decrypt(Ciphertext, key)
 print("Plaintext is: \n"+plaintext)
-=======
-print("Ciphertext is "+Ciphertext)
-plaintext = onetimepad.decrypt(Ciphertext, key)
-print("Plaintext is "+plaintext)
->>>>>>> origin/master
+
 
