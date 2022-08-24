5ae92b352e31b48ec4f63c7aac26ef717964feca
neeraj <neerajchowdary889@gmail.com>
2022-08-25 00:30:47 +0530

    Affine cipher, issue solved...

--- AffineCipher.py
+++ AffineCipher.py
@@ -5,14 +5,8 @@ message = input("Enter the message to Encrypt: ")
 message = message.lower()
 message = list(message)
 key = []
-def Keygeneration():
-    x1 = 0
-    while x1 < 2:
-        key.append(random.randint(1,9))
-        x1 = x1+1
-Keygeneration()
-a = int(2)
-b = int(7)
+a = 3
+b = 20
 print(a,b)
 temp = []
 for j in range(len(message)):
@@ -26,41 +20,48 @@ class encrypt():
     def Encryption(self):
         encrypttemp = 0
         for i in range(len(message)):
-            encrypttemp = (a*temp[i]+b)%26
+            encrypttemp = (((a)*(temp[i])+(b))%26)
             self.temp.append(encrypttemp)
             self.Encrypt.append(alphabets[self.temp[i]])
-            print('qq',encrypttemp)
         self.Encrypt = ''.join(map(str,self.Encrypt))
-        print(self.Encrypt)
-def gcd(x, y):
-    if (x == 0):
-        return y
-    return gcd(y % x, x)
-def phi(n):
-    result = 1
-    for i in range(2, n):
-        if (gcd(i, n) == 1):
-            result+=1
-    return result
-n = a
-n = phi(n)
+        print(f"Ciphertext : {self.Encrypt}")
+# def gcd(x, y):
+#     if (x == 0):
+#         return y
+#     return gcd(y % x, x)
+# def gcd(x, y):
+#     for x in range(1, y):
+#         if (((x % y) * (x % y)) % y == 1):
+#             return x
+#     return -1
+# def phi(n):
+#     result = 1
+#     for i in range(2, n):
+#         if (gcd(i, n) == 1):
+#             result+=1
+#     return result
+# n = a
+def modInverse(a, m):
+
+    for x in range(1, m):
+        if (((a%m) * (x%m)) % m == 1):
+            return x
+    return -1
+n = modInverse(a,26)
 print(n)
+
 class decrypt(encrypt):
     def Decryption(self):
         self.temp2 = []
         self.decrypt = []
         self.decrypt1 = []
         self.Encrypt = list(self.Encrypt)
-        print(self.Encrypt)
         j = 0
         for j in range(len(self.Encrypt)):
             self.temp2.append(alphabets.index(self.Encrypt[j]))
-            print(self.temp2)
-        print(self.temp2)
         for v in range(len(self.temp2)):
             decryption = (((self.temp2[v]-b)*(n))%26)
             self.decrypt.append(decryption)
-            print(self.decrypt)
         for k in range(len(self.decrypt)):
             self.decrypt1.append(alphabets[self.decrypt[k]])
         self.decrypt1 = ''.join(map(str,self.decrypt1))
@@ -69,5 +70,5 @@ Encrypt = decrypt()
 Encrypt.Encryption()
 Encrypt.Decryption()
 
-""" Can't figure it out whats going wrong here according to my caluculation the values from encryption is on point but
-something going wrong, will figure it out soon. Suspending for days due to techinal issues"""
+"""a values should be in certain range, in only that range this code works, dont know the issue. some calculation mistakes..."""
+"""NOT PERFECT...."""
